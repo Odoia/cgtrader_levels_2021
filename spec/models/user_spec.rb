9fc6531b7a3f0f7321e4c7980cea19bd285b224f
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2021-11-21 18:24:57 -0300

    development Fix a completes tests
    - now we can fix a pending tests
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,40 +1,39 @@
 require 'spec_helper'
 
 describe CgtraderLevels::User do
-  describe 'new user' do
+  context 'new user' do
     it 'has 0 reputation points' do
-      @user = CgtraderLevels::User.new
-      expect(@user.reputation).to eq(0)
+      user = CgtraderLevels::User.new
+      expect(user.reputation).to eq(0)
     end
 
     it "has assigned 'First level'" do
-      @level = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      @user = CgtraderLevels::User.new
+      level = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
+      user = CgtraderLevels::User.new
 
-      expect(@user.level).to eq(@level)
+      expect(user.level).to eq(level)
     end
   end
 
-  describe 'level up' do
+  context 'level up' do
     it "level ups from 'First level' to 'Second level'" do
-      @level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      @level_2 = CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
-      @user = CgtraderLevels::User.create!
+      level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
+      level_2 = CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
+      user = CgtraderLevels::User.create!
 
       expect {
-        @user.update_attribute(:reputation, 10)
-      }.to change { @user.reload.level }.from(@level_1).to(@level_2)
+        user.update_attribute(:reputation, 10)
+      }.to change { user.reload.level }.from(level_1).to(level_2)
     end
 
-    it "level ups from 'First level' to 'Second level'" do
-      @level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      @level_2 = CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
-      @level_3 = CgtraderLevels::Level.create!(experience: 13, title: 'Third level')
-      @user = CgtraderLevels::User.create!
+    it "level ups from 'First level' to 'Third level'" do
+      level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
+      level_3 = CgtraderLevels::Level.create!(experience: 13, title: 'Third level')
+      user = CgtraderLevels::User.create!
 
       expect {
-        @user.update_attribute(:reputation, 11)
-      }.to change { @user.reload.level }.from(@level_1).to(@level_2)
+        user.update_attribute(:reputation, 13)
+      }.to change { user.reload.level }.from(level_1).to(level_3)
     end
   end
 
