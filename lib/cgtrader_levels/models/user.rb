a92459a4d2b858ef4398e155bfa0b575a388fd3f
Tiago Henrique <tiagohenriqueodoia@gmail.com>
2021-11-21 21:25:04 -0300

    development Refactor tests
    -
    
    Signed-off-by: Tiago Henrique <tiagohenriqueodoia@gmail.com>

--- lib/cgtrader_levels/models/user.rb
+++ lib/cgtrader_levels/models/user.rb
@@ -1,61 +1,63 @@
-class CgtraderLevels::User < ActiveRecord::Base
-  attr_reader :level
+module CgtraderLevels
+  class User < ActiveRecord::Base
+    attr_reader :level, :coin_reward_by_reputation_hash, :tax_reduce_by_reputation_hash
 
-  after_initialize do
-    self.reputation = 0
+    after_initialize do
+      self.reputation = 0
 
-    matching_level = CgtraderLevels::Level.find_by(experience: reputation)
+      matching_level = CgtraderLevels::Level.find_by(experience: reputation)
 
-    if matching_level
-      self.level_id = matching_level.id
-      @level = matching_level
+      if matching_level
+        self.level_id = matching_level.id
+        @level = matching_level
+      end
     end
-  end
 
-  after_update :set_new_level
+    after_update :set_new_level
 
-  private
+    private
 
-  def set_new_level
-    matching_level = CgtraderLevels::Level.find_by(experience: reputation)
+    def set_new_level
+      matching_level = CgtraderLevels::Level.find_by(experience: reputation)
 
-    if matching_level
+      return unless matching_level
+
       update_privileges if level.experience < reputation
 
       self.level_id = matching_level.id
       @level = matching_level
     end
-  end
 
-  def update_privileges
-    coins_reward
-    tax_reduce
-  end
+    def update_privileges
+      coins_reward
+      tax_reduce
+    end
 
-  def coins_reward
-    return unless coin_reward_by_reputation_hash[reputation]
+    def coins_reward
+      return unless coin_reward_by_reputation_hash[reputation]
 
-    self.coins += coin_reward_by_reputation_hash[reputation]
-  end
+      self.coins += coin_reward_by_reputation_hash[reputation]
+    end
 
-  def coin_reward_by_reputation_hash
-    {
-      10 => 7
-    }
-  end
+    def coin_reward_by_reputation_hash
+      @coin_reward_by_reputation_hash ||= {
+        10 => 7
+      }
+    end
 
-  def tax_reduce
-    return unless tax_reduce_by_reputation_hash[reputation]
+    def tax_reduce
+      return unless tax_reduce_by_reputation_hash[reputation]
 
-    reduce_tax = tax_reduce_by_reputation_hash[reputation]
-    result = ((1 - reduce_tax) * tax)
+      reduce_tax = tax_reduce_by_reputation_hash[reputation]
+      result = ((1 - reduce_tax) * tax)
 
-    self.tax = result
-  end
+      self.tax = result.to_f
+    end
 
-  def tax_reduce_by_reputation_hash
-    {
-      10 => 0.01
-    }
+    def tax_reduce_by_reputation_hash
+      @tax_reduce_by_reputation_hash ||= {
+        10 => 0.01
+      }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,66 +1,63 @@
 require 'spec_helper'
 
-describe CgtraderLevels::User do
-  xcontext 'new user' do
+describe CgtraderLevels::User, type: :model do
+
+  let(:level1) { CgtraderLevels::Level.create!(experience: 0, title: 'First level') }
+  let(:level2) { CgtraderLevels::Level.create!(experience: 10, title: 'Second level') }
+  let(:level3) { CgtraderLevels::Level.create!(experience: 13, title: 'Third level') }
+  let(:user) { CgtraderLevels::User.create! }
+
+  context 'new user' do
+    before do
+      level1
+    end
+
     it 'has 0 reputation points' do
       user = CgtraderLevels::User.new
       expect(user.reputation).to eq(0)
     end
 
     it "has assigned 'First level'" do
-      level = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
       user = CgtraderLevels::User.new
 
-      expect(user.level).to eq(level)
+      expect(user.level).to eq(level1)
     end
   end
 
-  xcontext 'level up' do
-    it "level ups from 'First level' to 'Second level'" do
-      level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      level_2 = CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
-      user = CgtraderLevels::User.create!
+  context 'level up' do
+    before do
+      level1
+      level2
+      level3
+    end
 
-      expect {
-        user.update_attribute(:reputation, 10)
-      }.to change { user.reload.level }.from(level_1).to(level_2)
+    it "level ups from 'First level' to 'Second level'" do
+      expect { user.update_attribute(:reputation, 10) }
+        .to change { user.reload.level }.from(level1).to(level2)
     end
 
     it "level ups from 'First level' to 'Third level'" do
-      level_1 = CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      level_3 = CgtraderLevels::Level.create!(experience: 13, title: 'Third level')
-      user = CgtraderLevels::User.create!
-
-      require 'pry'; binding.pry
-      expect {
-        require 'pry'; binding.pry
-        user.update_attribute(:reputation, 13)
-      }.to change { user.reload.level }.from(level_1).to(level_3)
+      expect { user.update_attribute(:reputation, 13) }
+        .to change { user.reload.level }.from(level1).to(level3)
     end
   end
 
   context 'level up bonuses & privileges' do
-    it 'gives 7 coins to user' do
-      CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
-
-      user = CgtraderLevels::User.create!(coins: 1)
-
-      expect {
-        user.update_attribute(:reputation, 10)
-      }.to change { user.coins }.from(1).to(8)
+    before do
+      level1
+      level2
     end
 
-    it 'reduces tax rate by 1'do
-      CgtraderLevels::Level.create!(experience: 0, title: 'First level')
-      CgtraderLevels::Level.create!(experience: 10, title: 'Second level')
-
+    it 'gives 7 coins to user' do
       user = CgtraderLevels::User.create!(coins: 1)
-      require 'pry'; binding.pry
 
-      expect {
-        user.update_attribute(:reputation, 10)
-      }.to change { user.coins }.from(1).to(8)
+      expect { user.update_attribute(:reputation, 10) }
+        .to change { user.coins }.from(1).to(8)
+    end
+
+    it 'reduces tax rate by 1' do
+      expect { user.update_attribute(:reputation, 10) }
+        .to change { user.tax }.from(30).to(29.7)
     end
   end
 end
